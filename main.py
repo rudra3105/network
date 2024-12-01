9f7881af5108749dcb53377cf4c432b7f9612c03
Rudra Bhatt <105910217+rudra3105@users.noreply.github.com>
2024-12-01 21:12:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# Importing the script files
+from capture import capture_packets
+from anomaly_detection import detect_anomalies
+
+# Running the functions
+capture_packets()  # If this function is in capture.py
+detect_anomalies()  # If this function is in anomaly_detection.py
